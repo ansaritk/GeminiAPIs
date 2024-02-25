5760bdeaf2ee3fed7059e21f22bac62520c80e34
Ansari T K <Ansaritherkattil@gmail.com>
2024-02-26 00:00:39 +0100

    Fix Click Button Error

--- app.py
+++ app.py
@@ -4,7 +4,8 @@ from langchain.text_splitter import RecursiveCharacterTextSplitter
 import os
 from langchain_google_genai import GoogleGenerativeAIEmbeddings
 import google.generativeai as genai
-from langchain.vectorstores import FAISS
+#from langchain.vectorstores import FAISS
+from langchain_community.vectorstores import FAISS
 from langchain_google_genai import ChatGoogleGenerativeAI
 from langchain.chains.question_answering import load_qa_chain
 from langchain.prompts import PromptTemplate
@@ -12,6 +13,8 @@ from dotenv import load_dotenv
 
 load_dotenv()
 genai.configure(api_key=os.getenv('GOOGLE_API_KEY'))
+if 'processing_done' not in st.session_state:
+    st.session_state['processing_done'] = False
 
 model = genai.GenerativeModel('gemini-pro')
 def get_pdf_text(pdf_docs):
@@ -31,6 +34,7 @@ def get_text_chunks(text):
 def get_vector_store(text_chunks):
     embeddings = GoogleGenerativeAIEmbeddings(model = "models/embedding-001")
     vector_store = FAISS.from_texts(text_chunks, embedding=embeddings)
+
     vector_store.save_local("faiss_index")
 
 
@@ -54,40 +58,50 @@ def get_conversational_chain():
     return chain
 
 def user_input(user_question):
-    embeddings = GoogleGenerativeAIEmbeddings(model = "models/embedding-001")
-    
-    new_db = FAISS.load_local("faiss_index", embeddings)
-    docs = new_db.similarity_search(user_question)
+    if st.session_state.get('processing_done', True):
+        embeddings = GoogleGenerativeAIEmbeddings(model = "models/embedding-001")
+        
+        new_db = FAISS.load_local("faiss_index", embeddings)
+        docs = new_db.similarity_search(user_question)
 
-    chain = get_conversational_chain()
+        chain = get_conversational_chain()
+        response = chain(
+            {"input_documents":docs, "question": user_question}
+            , return_only_outputs=True)
 
-    
-    response = chain(
-        {"input_documents":docs, "question": user_question}
-        , return_only_outputs=True)
+        print(response)
+        st.write("Reply:\n", response["output_text"])
+    else:
+        st.error("Please upload PDF filesand and Click on the Confirm & Submit Button")
 
-    print(response)
-    st.write("Reply: ", response["output_text"])
+import logging
+# Set up logging for debugging
+logging.basicConfig(filename="faiss_index_log.txt", level=logging.DEBUG)
 
 def main():
-    st.set_page_config("Chat PDF")
-    st.header("Chat with PDF using Gemini💁")
+    st.set_page_config("Chat with PDF")
+    st.header("Chat with PDF (Beta mode V1.1)")
 
     user_question = st.text_input("Ask a Question from the PDF Files")
-
+    
     if user_question:
         user_input(user_question)
 
     with st.sidebar:
         st.title("Menu:")
-        pdf_docs = st.file_uploader("Upload your PDF Files and Click on the Submit & Process Button", accept_multiple_files=True)
-        if st.button("Submit & Process"):
-            with st.spinner("Processing..."):
-                raw_text = get_pdf_text(pdf_docs)
-                text_chunks = get_text_chunks(raw_text)
-                get_vector_store(text_chunks)
-                st.success("Done")
+        pdf_docs = st.file_uploader("Upload your PDF Files and Click on the Confirm & Submit Button", accept_multiple_files=True)
 
+        if st.button("Confirm & Submit"):
+            with st.spinner("Processing..."):
+                if pdf_docs:
+                    raw_text = get_pdf_text(pdf_docs)
+                    text_chunks = get_text_chunks(raw_text)
+                    # Delete existing faiss_index
+                    get_vector_store(text_chunks)
+                    st.success("Done")
+                    st.session_state['processing_done'] = True  # Set processing_done after successful processing
+                else:
+                    st.error("Please upload PDF files before clicking the button.")
 
 
 if __name__ == "__main__":
